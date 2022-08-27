1bf7cae3a8e2e627bd411594fffaa6f646835511
SumiAkter2 <akteralisha36@gmail.com>
2022-08-27 09:09:34 +0600

    insertone

--- index.js
+++ index.js
@@ -7,8 +7,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-//
-//
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dnshpdg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -20,10 +19,13 @@ async function run() {
   try {
     await client.connect();
     const collection = client.db("foodExpress").collection("users");
-    const user = { name: "sumi", email: "sumi@gmail.com", roll: "8" };
-    const result = await collection.insertOne(user);
 
-    console.log("connected");
+    app.post("/user", async (req, res) => {
+      const newUser = req.body;
+      console.log("received", newUser);
+      const result = await collection.insertOne(newUser);
+      res.send(result);
+    });
   } finally {
   }
 }
@@ -31,7 +33,7 @@ async function run() {
 run().catch(console.dir);
 
 app.get("/", (req, res) => {
-  res.send(" agency is running!");
+  res.send(" Node and Mongo recap is running!");
 });
 
 app.listen(port, () => {
